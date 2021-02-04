75d009a330817215c2007141913835941e48a7bb
Jacek Stec (JTK) <JTK@kmd.dk>
2021-02-04 14:32:36 +0100

    added fixture

--- BillTracker.Tests/BillTrackerWebApplicationFactory.cs
+++ BillTracker.Tests/BillTrackerWebApplicationFactory.cs
@@ -1,18 +1,19 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using BillTracker.Api;
 using BillTracker.Entities;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Xunit;
 
 namespace BillTracker.Tests
 {
     public class BillTrackerWebApplicationFactory : WebApplicationFactory<Startup>
     {
-
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder
@@ -30,6 +31,25 @@ namespace BillTracker.Tests
                     using var context = services.BuildServiceProvider().GetRequiredService<BillTrackerContext>();
                     context.Database.EnsureCreated();
                 });
+        }
+    }
+
+    public class BillTrackerFixture : IClassFixture<BillTrackerWebApplicationFactory>
+    {
+        private static readonly BillTrackerWebApplicationFactory Factory = new BillTrackerWebApplicationFactory();
+
+        internal BillTrackerWebApplicationFactory GetWebApplicationFactory() => Factory;
+
+        internal async Task<User> CreateUser()
+        {
+            using var scope = Factory.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<BillTrackerContext>();
+            var user = User.Create($"{Guid.NewGuid()}-xyz@syz.com", "pass", "Test", "Test");
+
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+
+            return user;
         }
     }
 }
