3fac3e77909921d6d28bcce4edcf167a6d801704
Jacek Stec (JTK) <JTK@kmd.dk>
2021-02-23 15:39:50 +0100

    added extension method for checking if user exists

---
+++ BillTracker/Entities/UsersDbSetExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BillTracker.Entities
+{
+    internal static class UsersDbSetExtensions
+    {
+        public static Task<bool> DoesExist(this DbSet<User> dbSet, Guid userId)
+        {
+            return dbSet.AnyAsync(x => x.Id == userId);
+        }
+    }
+}
