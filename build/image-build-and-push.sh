5bef82f36324fbcc6e202530666c59bd32bae8f0
Jacek Stec (JTK) <JTK@kmd.dk>
2021-04-12 12:33:13 +0200

    added building and pushing the image

---
+++ build/image-build-and-push.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# https://stackoverflow.com/a/20901614
+function dir_resolve() {
+  local dir=`dirname "$1"`
+  local file=`basename "$1"`
+  if [ "$file" = ".." ]; then
+    pushd "$1" &>/dev/null || return $? # On error, return error code
+    echo "`pwd -P`" # output full, link-resolved path with filename
+    popd &> /dev/null
+  else
+    pushd "$dir" &>/dev/null || return $? # On error, return error code
+    echo "`pwd -P`/$file" # output full, link-resolved path with filename
+    popd &> /dev/null
+  fi
+}
+
+# The full path to this script. https://stackoverflow.com/a/246128
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+dockerContextPath=$(dir_resolve "$SCRIPT_DIR/..")
+dockerFilePath=$(dir_resolve "$SCRIPT_DIR/../BillTracker.Api/Dockerfile")
+
+echo $dockerContextPath
+echo $dockerFilePath
+
+docker build -t "jaceks2106/billtracker-api:latest" -f $dockerFilePath $dockerContextPath
+
+docker push "jaceks2106/billtracker-api:latest"
