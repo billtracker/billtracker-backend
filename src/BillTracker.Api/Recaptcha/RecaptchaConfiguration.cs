58f2fd96c8c9bfe4222d165c2ec375f8262b16fc
Jacek Stec (JTK) <JTK@kmd.dk>
2021-04-13 21:12:52 +0200

    changed recaptcha configuration

--- src/BillTracker.Api/Recaptcha/RecaptchaConfiguration.cs
+++ src/BillTracker.Api/Recaptcha/RecaptchaConfiguration.cs
@@ -4,8 +4,11 @@
     {
         public const string SectionName = "Recaptcha";
 
-        public bool UseRecaptcha { get; init; }
-
         public string Secret { get; init; }
+
+        public bool UseRecaptcha
+        {
+            get => !string.IsNullOrEmpty(Secret);
+        }
     }
 }
