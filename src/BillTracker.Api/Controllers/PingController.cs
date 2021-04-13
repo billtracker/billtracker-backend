4147d3d1c5bbcb3291496a77d0c3ac5d166de33e
Jacek Stec (JTK) <JTK@kmd.dk>
2021-04-13 15:26:35 +0200

    Added PingController

---
+++ src/BillTracker.Api/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BillTracker.Api.Controllers
+{
+    [ApiController]
+    [Route("ping")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
