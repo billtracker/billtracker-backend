509e3f009f6f93e9d93fcf352f905cc3b9bfc32b
Jacek Stec (JTK) <JTK@kmd.dk>
2021-04-13 21:01:24 +0200

    allow anonymous on ping

--- src/BillTracker.Api/Controllers/PingController.cs
+++ src/BillTracker.Api/Controllers/PingController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BillTracker.Api.Controllers
 {
+    [AllowAnonymous]
     [ApiController]
     [Route("ping")]
     public class PingController : ControllerBase
